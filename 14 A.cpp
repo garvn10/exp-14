b79ddd4a7da2f359057878712481da7a29fbfab8
Garv Nandwana <149328353+garvn10@users.noreply.github.com>
2024-10-08 22:44:48 +0530

    Add files via upload

---
+++ 14 A.cpp
@@ -0,0 +1,29 @@
+//SHIVENDRA DWIVEDI
+//23070123122
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class uni
+{
+    public:
+    string uni = "symbiosis: ";
+    void discipline()
+    {
+        cout<<"engineering "<<endl;
+    }
+};
+
+class dep: public uni
+{
+    public:
+    string dept="electronics and telecommunication ";
+};
+
+int main()
+{
+    dep u1;
+    u1.discipline();
+    cout<<u1.uni+" "+u1.dept;
+}
